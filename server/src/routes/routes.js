11e76cb4afa2228cc9ae59eda6f943957fa3ad71
mbastaja <bastajam@gmail.com>
2021-10-11 23:26:29 +0200

    new changes to routes.js

---
+++ server/src/routes/routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const {body} = require('express-validator')
+const {register} = require('../controllers/registerController')
+// const {login} = require('../controllers/loginController')
+// const {getUser} = require('./controllers/getUserController')
+
+router.post('/register', [
+    body('username', "The username must be of minimum 5 characters lenght")
+    .notEmpty()
+    .escape()
+    .trim()
+    .isLength({min: 5}),
+    body('password', "The password must be of minimum 8 characters")
+    .notEmpty()
+    .trim()
+    .isLength({ min: 8}),
+], register)
+
+// router.post('login', [
+//     body('username', "Invalid username")
+//     .notEmpty()
+//     .escape()
+//     .trim()
+//     .isEmail(),
+//     body('password', "Invalid password")
+//     .notEmpty()
+//     .trim()
+//     .isLength({ min: 8 }),
+// ], login)
+
+// router.get('/getuser', getUser)
+
+module.exports = router
