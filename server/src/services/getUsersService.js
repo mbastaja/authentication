219b5940dc50bc691c7539fada07e456e36cd6ee
mbastaja <bastajam@gmail.com>
2021-10-13 13:01:40 +0200

    changes to getUsersService

--- server/src/services/getUsersService.js
+++ server/src/services/getUsersService.js
@@ -1,17 +1,14 @@
 const User = require('../models/models');
 
-module.exports = getAll
-
 function getAll(result) {
     try {
         User.getUsers(function (err, res) {
             if (err) throw err;
-            console.log('service');
             result(null, res)
         })
     }
     catch (e) {
-        throw Error('Error')
+        throw Error('Error listing all users')
     }
 }
 module.exports = getAll
