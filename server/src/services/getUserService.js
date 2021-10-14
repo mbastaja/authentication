bcc6d3271c596be1df0cce262d7b72cb4254d494
mbastaja <bastajam@gmail.com>
2021-10-14 17:04:34 +0200

    added try/catch

--- server/src/services/getUserService.js
+++ server/src/services/getUserService.js
@@ -1,7 +1,7 @@
 const User = require('../models/models');
 
 function getById(id, result) {
-    try{
+    try {
         User.getUserById(id, function (err, user) {
             if (err) throw err;
             result(null, user)
@@ -9,6 +9,6 @@ function getById(id, result) {
     }
     catch (e) {
         throw Error('Error getting specific user by ID')
-    }    
+    }
 }
 module.exports = getById
