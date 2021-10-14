d61d5ccc95c5d9354115c6462f60290002972472
mbastaja <bastajam@gmail.com>
2021-10-14 16:58:14 +0200

    added username and email check

--- server/src/controllers/registerController.js
+++ server/src/controllers/registerController.js
@@ -1,36 +1,50 @@
 const RegisterService = require('../services/registerService')
-const CheckService = require('../services/checkService')
+const CheckUsernameService = require('../services/checkUsernameService')
+const CheckEmailService = require('../services/checkEmailService')
 const validationService = require('../services/validation')
 
 
 
 exports.register = async (req, res, next) => {
-    
+
     // validationService(req, function (err, result) {
     //     console.log(req.body.username);
     //     if (err) throw err;
-        
+
     //     if (!result.isEmpty()) {
     //         return res.status(422).json({ errors: result.array() })
     //     }
     //     else {
-            CheckService(req.body.username, function (err, result) {
+    CheckUsernameService(req.body.username, function (err, result) {
+        if (err) {
+            throw err
+        }
+        else if (result[0]) {
+            return res.status(201).json({
+                message: `This username is already in use.`,
+            })
+        }
+        else {
+            CheckEmailService(req.body.email, function (err, result) {
                 if (err) {
                     throw err
                 }
                 else if (result[0]) {
                     return res.status(201).json({
-                        message: `This username is already in use`,
+                        message: `This email address is already in use.`
                     })
                 }
                 else {
                     RegisterService(req.body, function (err, user) {
                         if (err) throw err
-                        res.status(201).json({ error: false, message: `The user with ID: ${user} has been added successfully!`})
+                        res.status(201).json({ error: false, message: `The user with ID: ${user} has been added successfully!` })
 
                     })
                 }
             })
+
         }
+    })
+}
 //     })
 // }
