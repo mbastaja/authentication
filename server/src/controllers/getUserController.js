f65f7145a71710d2cebd944c8c0ef8b6229db694
mbastaja <bastajam@gmail.com>
2021-10-16 20:20:52 +0200

    removed comments/console.logs

--- server/src/controllers/getUserController.js
+++ server/src/controllers/getUserController.js
@@ -1,23 +1,18 @@
 const GetUserService = require('../services/getUserService')
-const GetTokenService = require('../services/getTokenService')
 
 exports.getUser = (req, res, next) => {
-    // GetTokenService(req, function (err, decoded) {
-    //     console.log(decoded +' nema');
+
     if (req.decoded) {
         GetUserService(req.decoded.id, function (err, result) {
             if (err) {
                 throw err
             }
             else {
-                console.log('milos');
                 res.status(200).json({ message: 'Fetch successful!', data: result })
             }
         })
     }
     else {
-        console.log('milo');
         res.status(422).json({ message: 'Please provide the token' })
     }
-    // })
 }
