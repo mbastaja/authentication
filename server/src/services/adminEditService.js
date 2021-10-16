81b43c02a35cedf89a38bdd3433f9505a47fb293
mbastaja <bastajam@gmail.com>
2021-10-16 19:56:18 +0200

    added adminEdit feature (controller and service)

---
+++ server/src/services/adminEditService.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/models')
+
+async function adminEditUsers(param, result) {
+
+    const hashPass = await bcrypt.hash(param.body.password, 12);
+
+    let params = {
+        username: param.body.username,
+        password: hashPass,
+        fullname: param.body.fullname,
+        email: param.body.email,
+        id: param.body.id
+    }
+    User.update(params, function (err, user) {
+        if (err) throw err
+        if(user){
+            result(null, user)
+        }
+        else{
+            result(err, null)
+        }
+    })
+
+}
+module.exports = adminEditUsers
