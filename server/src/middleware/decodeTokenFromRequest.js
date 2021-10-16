460e2a589e7ba865c960dc2d74db134631636644
mbastaja <bastajam@gmail.com>
2021-10-16 20:19:34 +0200

    added changes to decodeToken middleware

--- server/src/middleware/decodeTokenFromRequest.js
+++ server/src/middleware/decodeTokenFromRequest.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const GetTokenDbService = require('../services/refreshTokenDB/getTokenDbService')
 
 async function decodeTokenFromRequest(req, res, next) {
 
@@ -7,22 +8,53 @@ async function decodeTokenFromRequest(req, res, next) {
         !req.headers.authorization.split(' ')[1]) {
         return next()
     }
-    
+
     const token = req.headers.authorization.split(' ')[1];
-    
-    try{
-        const decoded = jwt.verify(token, 'the-super-strong-secret')
-        req.decoded = decoded
-        next()
-    }
-    catch(err){
-        throw Error('Expired user token, please login.')
+
+    if (token) {
+        jwt.verify(token, 'the-super-strong-secret', function (err, decoded) {
+            if (err) {
+                const payload = jwt.verify(token, 'the-super-strong-secret', { ignoreExpiration: true })
+                let userId = payload.id
+                GetTokenDbService(userId, function (err, refreshToken) {
+
+                    if (err) {
+                        return res.status(401).json({ success: false, message: 'No refresh token in database, please login.' })
+
+                    }
+                    else {
+
+                        jwt.verify(refreshToken[0].refreshToken, 'some-secret-refresh-token-stuff', function (error, valid) {
+
+                            if (error) {
+                                return res.status(401).json({ success: false, message: 'Refresh token expired, please login.' })
+
+                            }
+                            else {
+                                let payload = { "id": userId }
+
+                                let accessToken = jwt.sign(payload, 'the-super-strong-secret', { expiresIn: '2h' })
+
+
+                                jwt.verify(accessToken, 'the-super-strong-secret', function (err, decoded) {
+                                    req.decoded = decoded
+                                    res.tokens = JSON.stringify(accessToken)
+                                    next()
+                                })
+
+                            }
+                        })
+                    }
+
+                })
+            }
+            else {
+                req.decoded = decoded
+                next()
+            }
+        })
     }
 
 }
-
-module.exports = decodeTokenFromRequest
-
-
 
 module.exports = decodeTokenFromRequest
