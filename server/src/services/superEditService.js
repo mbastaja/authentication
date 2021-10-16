5d792f5f4ed9d866e192c9c3fd7a606957db507c
mbastaja <bastajam@gmail.com>
2021-10-16 20:11:09 +0200

    added feature for super admin to edit other users (controller and service)

---
+++ server/src/services/superEditService.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/models')
+
+async function superEditUsers(param, result) {
+
+    const hashPass = await bcrypt.hash(param.body.password, 12);
+
+    let params = {
+        username: param.body.username,
+        password: hashPass,
+        fullname: param.body.fullname,
+        email: param.body.email,
+        id: param.body.id,
+        role: param.body.role
+    }
+    User.update(params, function (err, user) {
+        if (err) throw err
+        if (user) {
+            result(null, user)
+        }
+        else {
+            result(err, null)
+        }
+    })
+
+}
+module.exports = superEditUsers
